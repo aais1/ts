91a19486bb22760f151b680d574210321648f882
AliAais <aaisali228@gmail.com>
2024-05-02 09:20:54 -0700

    learning ts - basic intro and basic types

---
+++ app.js
@@ -0,0 +1,15 @@
+// function add(a:number,b:number){
+//     console.log(a+b);
+// }
+// add(1,2);
+// function fullname(lname:string,fname:string){
+//     console.log(fname+" "+lname);
+// }
+// const n1=1.3 ;
+// fullname("Ali","Aais");
+var person = {
+    name: "Ali",
+    age: 18,
+    color: "white"
+};
+console.log(person.color);
