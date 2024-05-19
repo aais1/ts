2cdd69d9c970f3474b18dc22a47fbe9ba99078be
AliAais <aaisali228@gmail.com>
2024-05-19 06:09:03 -0700

    never,unkown

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+"use strict";
 // function add(a:number,b:number){
 //     console.log(a+b);
 // }
@@ -7,18 +8,37 @@ var Role;
     Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
     Role[Role["AUTHOR"] = 2] = "AUTHOR";
 })(Role || (Role = {}));
-var person = {
+const person = {
     name: "Ali",
     age: 18,
     color: "white",
     hobbies: ["gaming", "movies", "running"],
     role: Role.ADMIN
 };
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
+for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
 }
 function add(a, b) {
+    //console.log(a+b);not returning a number so this this will give error
     return a + b;
 }
 console.log(add(1, 2));
+//console.log(add('1','2')); this will give error because we have defined the type of a and b as number
+function callback(a, b, cb) {
+    const result = a + b;
+    cb(result);
+}
+const cb = (num) => {
+    console.log("In the callback :" + num * 2);
+};
+callback(1, 2, cb);
+let userName;
+let userInput;
+//userInput=userName;err
+if (typeof userName === 'string') {
+    userInput = userName;
+}
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError("An error occured", 500);
--- app.ts
+++ app.ts
@@ -51,3 +51,31 @@ function add (a:number,b:number):number {
 console.log(add(1,2));
 
 //console.log(add('1','2')); this will give error because we have defined the type of a and b as number
+
+function callback(a:number,b:number,cb:(num:number)=>void):void{
+    const result=a+b;
+    cb(result);
+}
+
+const cb=(num:number)=>{
+    console.log("In the callback :"+num*2);
+}
+
+callback(1,2,cb);
+
+
+
+let userName:unknown;
+let userInput:string;
+
+//userInput=userName;err
+
+if(typeof userName==='string'){
+    userInput=userName;
+}
+
+function generateError(message:string,code:number):never{
+    throw {message:message,errorCode:code};
+}
+
+generateError("An error occured",500);
